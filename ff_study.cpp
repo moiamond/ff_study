d6d52dbaf86d663dc6c7917acd541c2e0556ca9a
moiamond <moiamond@gmail.com>
2016-07-01 18:14:14 +0800

    Init ffmpeg study empty project

---
+++ ff_study.cpp
@@ -0,0 +1,26 @@
+// ff_study.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+extern "C"
+{
+#include "libavcodec/avcodec.h"
+#include "libavdevice/avdevice.h"
+#include "libavfilter/avfilter.h"
+#include "libavformat/avformat.h"
+#include "libavutil/avutil.h"
+#include "libpostproc/postprocess.h"
+#include "libswresample/swresample.h"
+#include "libswscale/swscale.h"
+#include "sdl/SDL.h"
+#include "sdl/SDL_thread.h"
+#undef main
+};
+
+
+int main()
+{
+    return 0;
+}
+
